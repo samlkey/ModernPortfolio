7f4b13be329eb35010b2b98e8c7fe36b6611764b
Sam Key <samkey726@gmail.com>
2025-03-28 22:27:55 +0000

    alterations to the experience screen

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,12 +6,16 @@
                 <p class="left-content">
                     Passionate Software Engineer with expertise in full-stack development, cyber security, and server management. Experienced in modern frameworks (React, Blazor, .NET) and cloud technologies (Docker, Azure).  
                 </p>
+
+                <p class="left-content">
+                    Proven ability to lead projects, develop business-critical systems, and solve complex technical challenges. Always eager to learn and apply cutting-edge technologies to enhance efficiency and performance.
+                </p>
             </div>
 
             <div class="lang-wrapper">
-                <p>
-                    Proven ability to lead projects, develop business-critical systems, and solve complex technical challenges. Always eager to learn and apply cutting-edge technologies to enhance efficiency and performance.
-                </p>
+                <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0"/>
+                </svg>
             </div>
         </div>
     </div>
--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -1,22 +1,77 @@
 <template>
     <div id="languageScroll">
         <div class="mainlang-wrapper">
-            <div class="lang-left">
-                <h1 class="lang-left-title">Experience,</h1>
+            <div class="lang-title">
+                <h1>Experience,</h1>
             </div>
-            <div class="lang-right">
-                <h1>Language,</h1>
-                <div class="lang-block">
-                    <LangBlock language="JavaScript" url="https://img.icons8.com/?size=100&id=108784&format=png&color=000000"/>
-                    <LangBlock language="C#" url="https://img.icons8.com/color/48/c-sharp-logo.png"/>
-                    <LangBlock language="Python" url="https://img.icons8.com/color/48/python--v1.png"/>          
+
+            <p class="langflavour">
+                BNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASL
+                BNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASLBNLASDLASLDASLDASL
+            </p>
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="langwrap">
+
+                <div class="lang-right">
+                    <h1>Language</h1>
+                    <div class="lang-block">
+                        <LangBlock language="JavaScript" url="https://img.icons8.com/?size=100&id=108784&format=png&color=000000"/>
+                        <LangBlock language="C#" url="https://img.icons8.com/color/48/c-sharp-logo.png"/>
+                        <LangBlock language="Python" url="https://img.icons8.com/color/48/python--v1.png"/>          
+                    </div>
+
+                    <div class="lang-block">
+                        <LangBlock language="C++" url="https://img.icons8.com/color/48/c-plus-plus-logo.png"/>
+                        <LangBlock language="HTML" url="https://img.icons8.com/color/48/html-5--v1.png"/>
+                        <LangBlock language="Java" url="https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png"/>           
+                    </div>
                 </div>
-                
-                <div class="lang-block">
-                    <LangBlock language="React" url="https://img.icons8.com/office/40/react.png"/>
-                    <LangBlock language="JS" url="https://img.icons8.com/?size=100&id=108784&format=png&color=000000"/>
-                    <LangBlock language="JS" url="https://img.icons8.com/?size=100&id=108784&format=png&color=000000"/>           
+
+                <div class="lang-right">
+                    <h1>Software</h1>
+                    <div class="lang-block">
+                        <LangBlock language="Blazor" url="https://img.icons8.com/?size=100&id=108784&format=png&color=000000"/>
+                        <LangBlock language="ASP.NET" url="https://img.icons8.com/color/48/c-sharp-logo.png"/>
+                        <LangBlock language=".NET" url="https://img.icons8.com/color/48/net-framework.png"/>          
+                    </div>
+
+                    <div class="lang-block">
+                        <LangBlock language="T-SQL" url="https://img.icons8.com/color/48/microsoft-sql-server.png"/>
+                        <LangBlock language="MongoDB" url="https://img.icons8.com/color/48/mongodb.png"/>
+                        <LangBlock language="PostgreSQL" url="https://img.icons8.com/plasticine/100/postgreesql.png"/>           
+                    </div>
                 </div>
+
+                <div class="lang-right">
+                    <h1>Frameworks</h1>
+                    <div class="lang-block">
+                        <LangBlock language="Express.js" url="https://img.icons8.com/office/40/express-js.png"/>
+                        <LangBlock language="jQuery" url="https://img.icons8.com/deco/48/jquery.png"/>
+                        <LangBlock language="Flask" url="https://img.icons8.com/nolan/64/flask.png"/>          
+                    </div>
+
+                    <div class="lang-block">
+                        <LangBlock language="React" url="https://img.icons8.com/office/40/react.png"/>
+                        <LangBlock language="Bootstrap" url="https://img.icons8.com/color/48/bootstrap--v2.png"/>
+                        <LangBlock language="Vue" url="https://img.icons8.com/color/48/vue-js.png"/>           
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
